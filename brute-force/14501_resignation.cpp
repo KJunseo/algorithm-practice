e9fbaa17223a1ec8095057e75d9edf1d4020715c
KJunseo <max9106@naver.com>
2020-03-20 12:08:51 +0900

    brute-force 14501 퇴사 Silver4

--- brute-force/14501_resignation.cpp
+++ brute-force/14501_resignation.cpp
@@ -5,13 +5,20 @@
     처음에는 시작점 하나씩 증가시켜가며, 최대 연속으로 일할 수 있는 날을 구했는데, 
     4번째 테스트케이스 같은 경우에 제대로 동작하지 않는 알고리즘이었다...
 
+   1. 해당 날에 상담 x -> 그 다음날
+
+   2. 해당 날에 상담 o -> 해당 상담을 하는데 걸리는 일 수 만큼 넘어감
+
     dp로 푸는 문제였다.
     거꾸로 탐색하면서, 최대 이익을 비교한다.
     만약 5일이 주어진 경우, 5일의 소요시간이 1일 아닌이상 최대값은 0이다.
     4일은 2일보다 많이 걸릴경우 최대값은 0이다.
     ...
 
-    솔직히 dp부분은 정말 많이 부족한것같다.. 완전 탐색으로 어떻게 푸는지는 모르겠다.
+    솔직히 dp부분은 정말 많이 부족한것같다..
+
+    +완전 탐색으로 풀려면 dfs를 사용한다.
+
 */
 #include <stdio.h>
 #include <algorithm>
@@ -22,6 +29,17 @@ using namespace std;
 int n;
 int t[MAX],p[MAX];
 int dp[MAX];
+
+// 해당 날부터 벌 수 있는 최대의 돈 
+int dfs(int day) {
+    if(day>=n+1) return 0;
+    int sum=0;
+
+    if(t[day]+day<=n+1) sum=max(sum, dfs(day+t[day])+p[day]); // 상담 o
+    if(day+1<n+1) sum=max(sum, dfs(day+1)); // 상담 x
+
+    return sum;
+}
 
 int main(void) {
     scanf("%d", &n);
@@ -36,7 +54,8 @@ int main(void) {
         else dp[i]=max(dp[i+1],dp[next]+p[i]); // 해당 날짜를 선택하지 않는 것이 큰 지, 선택하는 것이 큰 지 
     }
 
-    printf("%d", dp[1]);
+    printf("%d\n", dp[1]);
+    printf("%d\n", dfs(1));
 
     return 0;
 }
