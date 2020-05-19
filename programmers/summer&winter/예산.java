489253b1e95481d7e0b12f871b34f6358429cd6a
KJunseo <max9106@naver.com>
2020-05-19 11:23:57 +0900

    [summer/winter] 예산

---
+++ programmers/summer&winter/예산.java
@@ -0,0 +1,25 @@
+/*
+    그냥 오름차순 정렬해서 계산하면 되는거 아닌가..? 했는데 실제로 그랬다.
+    +1점 밖에 얻지 못했다 ㅎ..
+*/
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        
+        for(int i=0;i<d.length;i++) {
+            int current_cost = d[i];
+            
+            if(budget-current_cost<0) break;
+            
+            budget-=current_cost;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
